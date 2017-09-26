4f19d51e72e1e387f39164e0c4cc774068386c5a
Omar Segura <omarseguvi@gmail.com>
2017-09-25 23:53:42 -0600

    CheckMeals and it shows the time when it was pressed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams} from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,8 +8,53 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public isBreakfastStatusChanged: boolean;
+  public isMorningSnackStatusChanged: boolean;
+  public isLunchStatusChanged: boolean;
+  public isAfternoonSnackStatusChanged: boolean;
+  public isDinnerStatusChanged: boolean;
 
+  public breakfastTime: Date;
+  public morningSnackTime: Date;
+  public lunchTime: Date;
+  public afternoonSnackTime: Date;
+  public dinnerTime: Date;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.isBreakfastStatusChanged = false;
+    this.isMorningSnackStatusChanged = false;
+    this.isLunchStatusChanged = false;
+    this.isAfternoonSnackStatusChanged = false;
+    this.isDinnerStatusChanged = false;
   }
 
+  onBreakfastStatusChange(){
+    if(this.isBreakfastStatusChanged){
+      this.breakfastTime = new Date();
+    }
+  }
+
+  onMorningSnackStatusChange(){
+    if(this.isMorningSnackStatusChanged){
+      this.morningSnackTime = new Date();
+    }
+  }
+
+  onLunchStatusChange(){
+    if(this.isLunchStatusChanged){
+      this.lunchTime = new Date();
+    }
+  }
+
+  onAfternoonSnackStatusChange(){
+    if(this.isAfternoonSnackStatusChanged){
+      this.afternoonSnackTime = new Date();
+    }
+  }
+
+  onDinnerStatusChange(){
+    if(this.isDinnerStatusChanged){
+      this.dinnerTime =  new Date ();
+    }
+  }
 }
