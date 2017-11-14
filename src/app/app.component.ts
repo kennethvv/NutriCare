1c15ac329f070132110921d801c49a9886635a86
Omar Segura <omarseguvi@gmail.com>
2017-11-14 00:05:18 -0600

    SideBar implemented for every tab page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
  
-
 import { LoginPage } from '../pages/login/login';
 
 @Component({
@@ -25,11 +24,13 @@ export class MyApp {
       { title: 'Home', component: HomePage }
     ];
   }
-
+*/
   openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    console.log(page);
   }
-  */
+
+  logOut(){
+    console.log("Cerrar Sesion");
+  }
+  
 }
